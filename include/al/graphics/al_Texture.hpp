36d8cde326be49cc0b6dd9cac5ef6f44f9aa22ff
Sihwa Park <sihwapark@mat.ucsb.edu>
2020-05-29 19:38:40 -0700

    added a function to make a 3D texture

--- include/al/graphics/al_Texture.hpp
+++ include/al/graphics/al_Texture.hpp
@@ -187,9 +187,11 @@ class Texture : public GPUObject {
   void create2DArray(unsigned int width, unsigned int height, unsigned int depth,  
                 int internal = GL_RGBA8, unsigned int format = GL_RGBA, 
                 unsigned int type = GL_UNSIGNED_BYTE);
-  // TODO
-  // void create3D();
-
+  
+  void create3D(unsigned int width, unsigned int height, unsigned int depth,  
+                int internal = GL_RGBA8, unsigned int format = GL_RGBA, 
+                unsigned int type = GL_UNSIGNED_BYTE);
+  
   void createCubemap(
       unsigned int size, int internal = GL_RGBA8, unsigned int format = GL_RGBA,
       unsigned int type = GL_UNSIGNED_BYTE  // or GL_FLOAT is used
